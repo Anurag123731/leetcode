b38b362e108acabab303100a35f088d4ec35f550
Anurag Rawat <anuragrwt910@gmail.com>
2026-02-22 22:08:47 +0530

    Time: 3 ms (66.62%) | Memory: 19.2 MB (40.41%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    vector<TreeNode*> build(int start, int end) {
+        
+        vector<TreeNode*> trees;
+        
+        if(start > end) {
+            trees.push_back(NULL);
+            return trees;
+        }
+        
+        for(int i = start; i <= end; i++) {
+            
+            // Generate all left and right subtrees
+            vector<TreeNode*> leftTrees = build(start, i - 1);
+            vector<TreeNode*> rightTrees = build(i + 1, end);
+            
+            // Combine them
+            for(TreeNode* left : leftTrees) {
+                for(TreeNode* right : rightTrees) {
+                    
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left;
+                    root->right = right;
+                    
+                    trees.push_back(root);
+                }
+            }
+        }
+        
+        return trees;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        if(n == 0) return {};
+        return build(1, n);
+    }
+};
