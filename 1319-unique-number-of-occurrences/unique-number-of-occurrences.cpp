55a9f153807823597063c9c65310a15b35d04ab2
Anurag Rawat <anuragrwt910@gmail.com>
2026-08-22 15:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (71.51%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>m;
-        unordered_set<int>sk;
-        unordered_set<int>sv;
+        unordered_set<int>s;
         for(int i=0;i<arr.size();i++){
             m[arr[i]]++;
         }
         for(auto x:m){
-            sk.insert(x.first);
-            sv.insert(x.second);
+            if(s.find(x.second)!=s.end()){
+                return false;
+            }
+            s.insert(x.second);
         }
-        return sk.size()==sv.size();
+        return true;
     }
 };
