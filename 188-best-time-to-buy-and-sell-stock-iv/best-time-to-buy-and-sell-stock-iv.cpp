30d90b1e53291a8d9740ce8b2ec2cd4654911759
Anurag Rawat <anuragrwt910@gmail.com>
2026-09-25 22:48:50 +0530

    Time: 941 ms (5.13%) | Memory: 403.3 MB (5.66%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[1005][1005][2];
+    int dp[1005][105][2];
     int f(vector<int>prices,int i,int k,bool on){
         if(i==prices.size()) return 0;
         if (dp[i][k][on]!=-1) return dp[i][k][on];
